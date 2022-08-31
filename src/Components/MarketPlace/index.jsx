cb53e1586d6c9eebdd5c58cb5e063dda7f5db803
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2022-08-31 00:05:27 -0700

    WORKING

--- src/Components/MarketPlace/index.jsx
+++ src/Components/MarketPlace/index.jsx
@@ -1,37 +1,57 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ServiceModal from '../../View/Freelancer/AddService/ServicesModa'
-import "./styleMarketPlace.css"
+import ServiceModal from "../../View/Freelancer/AddService/ServicesModa";
+import "./styleMarketPlace.css";
+import useAuth from "../../util/Contex/useAuth";
+import Swal from "sweetalert2";
 
 const MarketPlace = ({ heading, showback }) => {
-
   const navigation = useNavigate();
-  const [show, Setshow] = useState(false)
-  const [ServiceType, SetServiceType] = useState('')
+  const [show, Setshow] = useState(false);
+  const [ServiceType, SetServiceType] = useState("");
+  let { auth } = useAuth();
   const AddService = (id, name) => {
-    Setshow(true)
-    SetServiceType({
-      name: name,
-      id: id,
-    })
-  }
+    if (auth?.email != undefined) {
+      Setshow(true);
+      SetServiceType({
+        name: name,
+        id: id,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Login To Continue",
+        // footer: '<a href="">Why do I have this issue?</a>',
+      });
+    }
+  };
+
+  console.log("auth", auth);
 
   return (
     <div className="xl:container xl:mx-0 mx-8">
       <div className="flex flex-wrap text-center">
-        {showback == true ?
+        {showback == true ? (
           <img
             onClick={() => navigation(-1)}
             loading="lazy"
             className="text-3xl my-20 font-bold"
-            src='https://firebasestorage.googleapis.com/v0/b/clancers-inc.appspot.com/o/images%2Fback.png?alt=media&token=6844150b-43c0-43d5-b8f0-80a752d90662' style={{ height: 40, width: 40, marginRight: 40 }} />
-          : null}
-        <h2 className="text-3xl my-20 font-bold">{heading ? heading : 'Explore the marketplace'}</h2>
+            src="https://firebasestorage.googleapis.com/v0/b/clancers-inc.appspot.com/o/images%2Fback.png?alt=media&token=6844150b-43c0-43d5-b8f0-80a752d90662"
+            style={{ height: 40, width: 40, marginRight: 40 }}
+          />
+        ) : null}
+        <h2 className="text-3xl my-20 font-bold">
+          {heading ? heading : "Explore the marketplace"}
+        </h2>
       </div>
       <div>
         <ul className="flex flex-wrap text-center">
-          <li onClick={() => AddService(1, 'Graphics & Design')} className="mb-14 w-1/2 md:w-1/3  lg:w-1/4 xl:w-1/5 col-auto " >
-            <a className="image"  >
+          <li
+            onClick={() => AddService(1, "Graphics & Design")}
+            className="mb-14 w-1/2 md:w-1/3  lg:w-1/4 xl:w-1/5 col-auto "
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className=" w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -43,8 +63,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(2, 'Digital Marketing')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 col-span-1">
-            <a className="image" >
+          <li
+            onClick={() => AddService(2, "Digital Marketing")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 col-span-1"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -56,8 +79,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(3, 'Writing & Translation')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(3, "Writing & Translation")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -65,12 +91,18 @@ const MarketPlace = ({ heading, showback }) => {
                 alt="Writing & Translation"
               />
               <div className="underLine"></div>
-              <p className="mt-3 text-base text-black"> Writing & Translation</p>
+              <p className="mt-3 text-base text-black">
+                {" "}
+                Writing & Translation
+              </p>
             </a>
           </li>
 
-          <li onClick={() => AddService(4, 'ideo & Animation')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(4, "ideo & Animation")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -82,8 +114,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(5, 'Music & Audio')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(5, "Music & Audio")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -95,8 +130,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(6, 'Programming & Tech')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(6, "Programming & Tech")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -108,8 +146,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(7, 'Business')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(7, "Business")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -121,8 +162,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(8, 'Lifestyle')} className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(8, "Lifestyle")}
+            className="mb-14 w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:w-1/5 m-auto "
@@ -134,8 +178,11 @@ const MarketPlace = ({ heading, showback }) => {
             </a>
           </li>
 
-          <li onClick={() => AddService(9, 'Data')} className="mb-14 w-1/2 md:w-1/3 md:mt-3  xl:mt-0  lg:w-1/4 xl:w-1/5">
-            <a className="image" >
+          <li
+            onClick={() => AddService(9, "Data")}
+            className="mb-14 w-1/2 md:w-1/3 md:mt-3  xl:mt-0  lg:w-1/4 xl:w-1/5"
+          >
+            <a className="image">
               <img
                 loading="lazy"
                 className="w-1/3 lg:w-1/4 xl:mt-3 pb-1 xl:w-1/5 m-auto "
